8f275f4df021ae4e90fbb65116aeee6ea0a3dfc9
marsiwiec <marsiwiec@users.noreply.github.com>
2025-08-28 21:51:07 +0200

    Switch to stylix theme.

--- config.el
+++ config.el
@@ -32,6 +32,10 @@
 (use-package org-modern
   :custom (org-modern-star 'replace))
 
+;; Theme
+(setq custom-theme-directory "~/.emacs.d/themes")
+(setq doom-theme 'doom-stylix)
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -40,7 +44,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+;; (setq doom-theme 'doom-vibrant)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
